6de1c8e0cea8ae43aceab2851891ee3068f3bb29
Robert Stolz <robert.stolz87@gmail.com>
2025-05-23 11:05:31 +0200

    render setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>WordLinkHub</h1>
+      <h1>Word Link Hub</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
