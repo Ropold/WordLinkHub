ac6230cb6e91afe57edec7dffcf82466d040e647
Robert Stolz <robert.stolz87@gmail.com>
2025-05-30 13:05:46 +0200

    navbar setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,11 @@ import ProtectedRoute from "./components/ProtectedRoute.tsx";
 import axios from "axios";
 import Welcome from "./components/Welcome.tsx";
 import Profile from "./components/Profile.tsx";
+import type {UserDetails} from "./components/model/UserDetailsModel.ts";
 
 export default function App() {
     const [user, setUser] = useState<string>("anonymousUser");
-
+    const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
 
 
     function getUser() {
@@ -25,19 +26,30 @@ export default function App() {
             });
     }
 
+    function getUserDetails() {
+        axios.get("/api/users/me/details")
+            .then((response) => {
+                setUserDetails(response.data as UserDetails);
+            })
+            .catch((error) => {
+                console.error(error);
+                setUserDetails(null);
+            });
+    }
+
     useEffect(() => {
         getUser();
     }, []);
 
   return (
       <>
-          <Navbar />
+          <Navbar getUser={getUser} getUserDetails={getUserDetails} user={user}/>
           <Routes>
               <Route path="*" element={<NotFound />} />
               <Route path="/" element={<Welcome />} />
 
               <Route element={<ProtectedRoute user={user} />}>
-                  <Route path="/profile/*" element={<Profile />} />
+                  <Route path="/profile/*" element={<Profile user={user} userDetails={userDetails} />} />
               </Route>
           </Routes>
           <Footer />
