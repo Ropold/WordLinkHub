61c8deffe531ebafb34ae6f9a9a547d901e95e31
Robert Stolz <robert.stolz87@gmail.com>
2025-06-03 18:57:42 +0200

    start highscore

--- frontend/src/components/HighScore.tsx
+++ frontend/src/components/HighScore.tsx
@@ -1,5 +1,51 @@
+import type {HighScoreModel} from "./model/HighScoreModel.ts";
+import "./styles/HighScore.css"
+import axios from "axios";
+import {useState} from "react";
 
-export default function HighScore(){
+type HighScoreProps = {
+    highScore: HighScoreModel[];
+    getHighScore: () => void;
+}
+
+const formatDate = (date: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+    };
+    return new Date(date).toLocaleDateString("de-DE", options);
+};
+
+export default function HighScore(props: Readonly<HighScoreProps>) {
+
+    const [githubUsernames, setGithubUsernames] = useState<Map<string, string>>(new Map());
+
+    function fetchGithubUsernames(highScores: HighScoreModel[]) {
+        const uniqueIds = new Set(
+            highScores
+                .filter(score => score.githubId !== "anonymousUser")
+                .map(score => score.githubId)
+        );
+
+        const newUsernames = new Map(githubUsernames);
+
+        uniqueIds.forEach(async (id) => {
+            if (!newUsernames.has(id)) {
+                axios.get(`https://api.github.com/user/${id}`)
+                    .then((response) => {
+                        newUsernames.set(id, response.data.login);
+                        setGithubUsernames(new Map(newUsernames));
+                    })
+                    .catch((error) => {
+                        console.error(`Error fetching GitHub user ${id}:`, error);
+                    });
+            }
+        });
+    }
+
     return(
         <h2>HighScore</h2>
     )
